31ca09b83904f152d91f16626ce99c5ed8b93bb7
vishal kharkya <109803237+vishal1015@users.noreply.github.com>
2024-07-27 20:31:26 +0530

    Update authController.js

--- Controller/authController.js
+++ Controller/authController.js
@@ -129,47 +129,6 @@ module.exports.forgot_pass_post = async(req,res)=>{
     console.log(error);
    }
 }
-// module.exports.requestPasswordReset = async (req, res) => {
-//   const { email } = req.body;
-//   try {
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(400).json({ error: "Email not found" });
-//     }
-   
-//     // Generate token
-//     const token = crypto.randomBytes(32).toString("hex");
-//     user.resetPasswordToken = token;
-//     user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
-//     await user.save();
-
-//     // Send email
-//     const transporter = nodemailer.createTransport({
-//       service: "Gmail",
-//       secure: true,
-//       auth: { user: process.env.MY_GMAIL, pass: process.env.MY_PASSWORD },
-//     });
-
-//     const mailOptions = {
-//       to: user.email,
-//       from: "vkharkya@gmail.com",
-//       subject: "Password Reset",
-//       text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
-//         Please click on the following link, or paste this into your browser to complete the process:\n\n
-//         http://${req.headers.host}/reset/${token}\n\n
-//         If you did not request this, please ignore this email and your password will remain unchanged.\n`,
-//     };
-
-//     await transporter.sendMail(mailOptions, (err, response) => {
-//       if (err) {
-//         return res.status(500).json({ error: "Error sending email" });
-//       }
-//       res.status(200).json({ message: "Reset link sent on your gamil" });
-//     });
-//   } catch (err) {
-//     res.status(500).json({ error: "Server error" });
-//   }
-// };
 
 module.exports.resetPassword_post = async (req, res) => {
  
